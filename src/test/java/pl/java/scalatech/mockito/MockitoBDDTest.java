4dd9521e7b9de14550274fdc19b730fdf7247699
Slawomir Borowiec <przodownik@tlen.pl>
2015-07-22 15:12:39 +0200

    mock test

--- src/test/java/pl/java/scalatech/mockito/MockitoBDDTest.java
+++ src/test/java/pl/java/scalatech/mockito/MockitoBDDTest.java
@@ -1,14 +1,14 @@
 package pl.java.scalatech.mockito;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import static org.mockito.BDDMockito.given;
 
 import java.math.BigDecimal;
 
 import org.assertj.core.util.Lists;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 
 import pl.java.scalatech.domain.Skill;
@@ -16,17 +16,18 @@ import pl.java.scalatech.domain.User;
 import pl.java.scalatech.repository.SearchUserRepository;
 
 @RunWith(MockitoJUnitRunner.class)
-public class MockitoTest {
+public class MockitoBDDTest {
+    @Mock
+    private SearchUserRepository searchUserRepository;
 
     @Test
     public void shouldFindByLoginWork() {
-        //given + when
-        SearchUserRepository searchUserRepository = mock(SearchUserRepository.class);
-        when(searchUserRepository.findByLogin("przodownik")).then(
+        //given
+        given(searchUserRepository.findByLogin("przodownik")).willAnswer(
                 invocation -> Lists.newArrayList(User.builder().id(1l).age(36).salary(BigDecimal.valueOf(20)).skill(Skill.JAVA).login("przodownik").build()));
-
+        //when
         User expected = User.builder().id(1l).age(36).salary(BigDecimal.valueOf(20)).skill(Skill.JAVA).login("przodownik").build();
-        //than
+        //then
         assertThat(searchUserRepository.findByLogin("przodownik")).isEqualTo(Lists.newArrayList(expected));
     }
 
